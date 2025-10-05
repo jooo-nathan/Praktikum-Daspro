07b92a4340398bb026201b9e9f81b7a12a38b26f
jooo-nathan <jonathanharyono4@gmail.com>
2025-10-05 17:44:48 +0700

    Modifikasi Percobaan 1

--- Jobsheet 5/ifCetakKRS14.java
+++ Jobsheet 5/ifCetakKRS14.java
@@ -12,5 +12,8 @@ public class ifCetakKRS14 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         }
+        else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
     } 
 }
