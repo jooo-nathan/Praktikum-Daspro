ce7a9987dc677592525a66183e7a3326c85e4613
jooo-nathan <jonathanharyono4@gmail.com>
2025-11-02 15:48:05 +0700

    Tugas 2

---
+++ Jobsheet 8/Tugas2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Masukkan nilai n : ");
+        int n = sc.nextInt();
+
+        float temp = n;
+        int jDigit = 1;
+        if (temp < 10) {
+            jDigit = 1;
+        }
+        else {
+            while (temp >= 10) {
+                temp /= 10;
+                jDigit++;
+            }
+        }
+        // System.out.println(jDigit);
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1 || i == n) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.printf("%d", n);
+                }
+            }
+            else {
+                for (int j = 1; j <= n; j++) {
+                    if (j == 1 || j == n) {
+                        System.out.printf("%d", n);
+                    }
+                    else {
+                        for (int k = 1; k <= jDigit; k++) {
+                            System.out.print(" ");
+                        }
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
