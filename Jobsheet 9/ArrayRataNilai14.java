f1f5d8d53b73378bf3279769d9094996465e8716
jooo-nathan <jonathanharyono4@gmail.com>
2025-11-04 12:17:38 +0700

    Modifikasi Percobaan 3

--- Jobsheet 9/ArrayRataNilai14.java
+++ Jobsheet 9/ArrayRataNilai14.java
@@ -1,23 +1,36 @@
 import java.util.Scanner;
-
 public class ArrayRataNilai14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        double totalL = 0, totalTL = 0;
+        double rata2L, rata2TL;
+        int lulus = 0, tlulus = 0;
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+
+        for (int i = 0; i < jumlahMhs; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+        for (int i = 0; i < jumlahMhs; i++) {
+            if (nilaiMhs[i] > 70) {
+                totalL += nilaiMhs[i];
+                lulus++;
+            }
+            else {
+                totalTL += nilaiMhs[i];
+                tlulus++;
+            }
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("\nMahasiswa yang lulus ada sebanyak " + lulus + " orang");
+        
+        System.out.printf("Rata-rata nilai lulus = %.1f\n", totalL/lulus);
+        System.out.printf("Rata-rata nilai tidak lulus = %.1f", totalTL/tlulus);
+        //rata2 = total/nilaiMhs.length;
+        // System.out.println("Rata-rata nilai = " + rata2);
     }
 }
