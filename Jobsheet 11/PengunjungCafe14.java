2d1ceac00b9efd8b8434f7950beb68d61662e3ff
jooo-nathan <jonathanharyono4@gmail.com>
2025-12-01 12:11:20 +0700

    Modifikasi Percobaan 4

--- Jobsheet 11/PengunjungCafe14.java
+++ Jobsheet 11/PengunjungCafe14.java
@@ -1,13 +1,14 @@
 public class PengunjungCafe14 {
     public static void daftarPengunjung(String ...namaPengunjung) {
         System.out.println("Daftar nama pengunjung : ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String temp : namaPengunjung) {
+            System.out.println("- " + temp);
         }
     }
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
         daftarPengunjung("Andi");
         daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+        daftarPengunjung();
     }
 }
